a4bc3d24498fdfa132dd300f0fe0ad0e07e13191
Louie Perez <louie.perez3@gmail.com>
2014-02-05 15:21:44 -0800

    Initial Classes Created

---
+++ spec/AnimalSpec.js
@@ -0,0 +1,20 @@
+describe("Animal", function() {
+  var animal;
+
+  beforeEach(function() {
+    animal = new Animal();
+    land = new Land();
+    land.growGrass();
+  });
+
+  it("should start hungry", function() {
+    expect(animal.isHungry()).toEqual(true);
+  });
+
+  it("should eat grass", function() {
+    initialHunger = animal.satiationLevel
+    animal.eat(land);
+    expect(animal.satiationLevel).toBeGreaterThan(initialHunger);
+  });
+
+});
